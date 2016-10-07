cceba1a9132d0ea124c76d3987feb51569ac2317
KristianMariyanov <kristian.mariyanov@gmail.com>
2016-10-07 16:44:58 +0300

    Added common services project with mapping service

---
+++ Services/LaptopListingSystem.Services.Common/Contracts/IMappingService.cs
@@ -0,0 +1,13 @@
+﻿namespace LaptopListingSystem.Services.Common.Contracts
+{
+    using System.Linq;
+
+    public interface IMappingService
+    {
+        TDestination Map<TDestination>(object source);
+
+        void Map<TSource, TDestination>(TSource source, TDestination destination);
+
+        IQueryable<TDestination> MapCollection<TDestination>(IQueryable source, object parameters = null);
+    }
+}
---
+++ Services/LaptopListingSystem.Services.Common/Mapping/AutoMapperMappingService.cs
@@ -0,0 +1,27 @@
+﻿namespace LaptopListingSystem.Services.Common.Mapping
+{
+    using System.Linq;
+
+    using AutoMapper;
+
+    using LaptopListingSystem.Services.Common.Contracts;
+    using AutoMapper.QueryableExtensions;
+
+    public class AutoMapperMappingService : IMappingService
+    {
+        private readonly IMapper mapper;
+
+        public AutoMapperMappingService(IMapper mapper)
+        {
+            this.mapper = mapper;
+        }
+
+        public TDestination Map<TDestination>(object source) => this.mapper.Map<TDestination>(source);
+
+        public void Map<TSource, TDestination>(TSource source, TDestination destination) =>
+            this.mapper.Map(source, destination);
+
+        public IQueryable<TDestination> MapCollection<TDestination>(IQueryable source, object parameters = null) =>
+            source.ProjectTo<TDestination>(this.mapper.ConfigurationProvider, parameters);
+    }
+}

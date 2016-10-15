36abcd7e6392f45514913bed308a58658edc8662
KristianMariyanov <kristian.mariyanov@gmail.com>
2016-10-15 09:36:40 +0300

    Renamed and improve laptop service. Add base service

---
+++ Web/LaptopListingSystem.Web/wwwroot/app/services/base.service.ts
@@ -0,0 +1,17 @@
+﻿import { Injectable } from '@angular/core';
+import { Headers, RequestOptions } from '@angular/http';
+
+@Injectable()
+export class BaseService {
+    getAuthorizationHeader(): Headers {
+        var currentUser = JSON.parse(localStorage.getItem('currentUser'));
+        let token = currentUser && currentUser.token;
+        let header = new Headers({ 'Authorization': 'Bearer ' + token });
+        return header;
+    }
+
+    getFormContentTypeHeader(): Headers {
+        let header = new Headers({ 'Content-Type': 'application/x-www-form-urlencoded' });
+        return header;
+    }
+}
---
+++ Web/LaptopListingSystem.Web/wwwroot/app/services/laptop.service.ts
@@ -0,0 +1,68 @@
+﻿import { Injectable } from '@angular/core';
+import { Http, Response, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+
+import { Laptop } from '../laptop';
+import { LaptopDetails } from '../laptop-details';
+import { BaseService } from './base.service';
+import './rxjs-operators';
+
+@Injectable()
+export class LaptopService extends BaseService {
+    private laptopsBaseUrl = 'api/laptops/';
+    private addCommentUrl = 'api/comments';
+    private addVoteUrl = 'api/votes';
+
+    constructor(private http: Http) { super(); }
+
+    getLaptops(): Observable<Laptop[]> {
+        return this.http.get(this.laptopsBaseUrl)
+            .map(this.extractData)
+            .catch(this.handleError);
+    }
+
+    getLaptop(id: number): Observable<LaptopDetails> {
+        return this.http.get(this.laptopsBaseUrl + id)
+            .map(this.extractData)
+            .catch(this.handleError);
+    }
+
+    addComment(content: string, laptopId: number): Observable<any> {
+        let headers = this.getAuthorizationHeader();
+        headers.append('Content-Type', 'application/x-www-form-urlencoded');
+        let options = new RequestOptions({ headers: headers });
+        return this.http.post(this.addCommentUrl, `content=${content}&laptopId=${laptopId}`, options)
+            .map(this.extractData)
+            .catch(this.handleError);
+    }
+
+    addVote(laptopId: number): Observable<any> {
+        let headers = this.getAuthorizationHeader();
+        headers.append('Content-Type', 'application/x-www-form-urlencoded');
+        let options = new RequestOptions({ headers: headers });
+        return this.http.post(this.addVoteUrl, `laptopId=${laptopId}`, options)
+            .map(this.extractData)
+            .catch(this.handleError);
+    }
+
+    filterLaptops(term: string, order: string): Observable<Laptop[]> {
+        let headers = this.getAuthorizationHeader();
+        let options = new RequestOptions({ headers: headers });
+        return this.http.get(`${this.laptopsBaseUrl}filter?term=${term}&order=${order}`, options)
+            .map(this.extractData)
+            .catch(this.handleError);
+    }
+
+    private extractData(res: Response) {
+        return res.json();
+    }
+
+    private handleError(error: any) {
+        // In a real world app, we might use a remote logging infrastructure
+        // We'd also dig deeper into the error to get a better message
+        let errMsg = (error.message) ? error.message :
+            error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+        console.error(errMsg); // log to console instead
+        return Observable.throw(errMsg);
+    }
+}

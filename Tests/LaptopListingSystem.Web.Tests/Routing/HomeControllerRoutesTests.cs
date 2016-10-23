62df3d55e3cc36d905b81fea38ed5ff8c94612f9
KristianMariyanov <kristian.mariyanov@gmail.com>
2016-10-23 23:17:51 +0300

    Added test project

---
+++ Tests/LaptopListingSystem.Web.Tests/Routing/HomeControllerRoutesTests.cs
@@ -0,0 +1,22 @@
+﻿namespace LaptopListingSystem.Web.Tests.Routing
+{
+    using LaptopListingSystem.Web.Controllers;
+
+    using MyTested.AspNetCore.Mvc;
+
+    using Xunit;
+
+    public class VotesControllerRoutesTests
+    {
+        [Fact]
+        public void GetAddressAndPaymentShouldBeRoutedCorrectly()
+        {
+            MyMvc
+                .Routing()
+                .ShouldMap(request => request
+                    .WithLocation("/api/votes/1")
+                    .WithMethod("POST"))
+                .To<VotesController>(c => c.Post(1));
+        }
+    }
+}

f8ec873b15d2b426997223bc530c0792c32ff257
KristianMariyanov <kristian.mariyanov@gmail.com>
2016-10-25 21:00:45 +0300

    Added Tests for laptopsController routes

---
+++ Tests/LaptopListingSystem.Web.Tests/Routing/LaptopsControllerRoutesTests.cs
@@ -0,0 +1,60 @@
+﻿namespace LaptopListingSystem.Web.Tests.Routing
+{
+    using LaptopListingSystem.Web.Controllers;
+    using LaptopListingSystem.Web.InputModels.Comments;
+
+    using Microsoft.AspNetCore.Http;
+
+    using MyTested.AspNetCore.Mvc;
+
+    using Xunit;
+
+    public class LaptopsControllerRoutesTests
+    {
+        [Fact]
+        public void GetLaptopsShouldBeRoutedCorrectly()
+        {
+            MyMvc
+                .Routing()
+                .ShouldMap(request => request
+                    .WithLocation("/api/laptops")
+                    .WithMethod("Get"))
+                .To<LaptopsController>(c => c.Get());
+        }
+
+        [Fact]
+        public void GetLaptopShouldBeRoutedCorrectly()
+        {
+            MyMvc
+                .Routing()
+                .ShouldMap(request => request
+                    .WithLocation("/api/laptops/1")
+                    .WithMethod("Get"))
+                .To<LaptopsController>(c => c.Get(1));
+        }
+
+        [Fact]
+        public void FilterLaptopsShouldBeRoutedCorrectly()
+        {
+            MyMvc
+                .Routing()
+                .ShouldMap(request => request
+                    .WithLocation("/api/laptops/filter")
+                    .WithQueryString("?term=term&order=order")
+                    .WithMethod("Get"))
+                .To<LaptopsController>(c => c.Filter("term", "order"));
+        }
+
+        [Fact]
+        public void GetLaptopsWithPagingShouldBeRoutedCorrectly()
+        {
+            MyMvc
+                .Routing()
+                .ShouldMap(request => request
+                    .WithLocation("/api/laptops/all")
+                    .WithQueryString("?page=2")
+                    .WithMethod("Get"))
+                .To<LaptopsController>(c => c.All(2));
+        }
+    }
+}
